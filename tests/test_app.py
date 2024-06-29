e76f4689cd62807df489e8cea3d128275631cb9c
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2024-06-29 11:50:25 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -32,18 +34,19 @@ def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'John Doe',
-                'email': 'john@teste.com',
-            },
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_users(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_users(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -60,7 +63,7 @@ def test_update_users(client):
     }
 
 
-def test_delete_users(client):
+def test_delete_users(client, user):
     response = client.delete('/users/1')
 
     assert response.json() == {
