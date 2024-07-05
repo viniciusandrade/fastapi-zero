6943e61593349e46664d6a731668dc67617c7c7d
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2024-07-05 16:06:46 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,13 +3,6 @@ from http import HTTPStatus
 from fast_zero.schemas import UserPublic
 
 
-def test_root_deve_retornar_ok_e_ola_mundo(client):
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
-
-
 def test_create_user(client):
     # Enviando dados no formato UserSchema
     response = client.post(
@@ -46,9 +39,10 @@ def test_read_users_with_user(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_users(client, user):
+def test_update_users(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'John Doe Updated',
             'email': 'john@teste.com',
@@ -63,9 +57,33 @@ def test_update_users(client, user):
     }
 
 
-def test_delete_users(client, user):
-    response = client.delete('/users/1')
+def test_delete_users(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.json() == {
         'message': 'User deleted',
     }
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert token['token_type'] == 'Bearer'
+    assert 'access_token' in token
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        '/users/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
